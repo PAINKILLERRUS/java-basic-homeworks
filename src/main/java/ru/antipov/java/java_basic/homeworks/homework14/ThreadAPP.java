8ac2c42cc0f552fe696e782d6307a3443ca2d353
Ivan Antipov <ivrusbash@gmail.com>
2024-04-09 14:47:08 +0300

    ДЗ скорректировано в соответствии с замечаниями.

--- src/main/java/ru/antipov/java/java_basic/homeworks/homework14/ThreadAPP.java
+++ src/main/java/ru/antipov/java/java_basic/homeworks/homework14/ThreadAPP.java
@@ -1,72 +1,47 @@
 package ru.antipov.java.java_basic.homeworks.homework14;
 
-import java.util.Arrays;
-
 /**
  * Задал количество элементов в массиве при его создании равное 1_000_000.
  * При попытке создания массива в 100_000_000 выпадает ошибка о недотстатке памяти, слабое железо
  */
 
-public class ThreadAPP implements Runnable {
+public class ThreadAPP {
+
     public static void main(String[] args) throws InterruptedException {
-        createMultiThreadArray(new double[1_000_000]);
+
+        Thread[] threads = new Thread[4];
+        double[] result = new double[1_000_000];
+        for (int i = 0; i < threads.length; i++) {
+            threads[0] = new Thread(() -> fillDoubleArray(result, 0, 250_000));
+            threads[1] = new Thread(() -> fillDoubleArray(result, 250_000, 500_000));
+            threads[2] = new Thread(() -> fillDoubleArray(result, 500_000, 750_000));
+            threads[3] = new Thread(() -> fillDoubleArray(result, 750_000, 1_000_000));
+        }
+        long executionTime = System.currentTimeMillis();
+        threads[0].start();
+        threads[1].start();
+        threads[2].start();
+        threads[3].start();
+        threads[0].join();
+        threads[1].join();
+        threads[2].join();
+        threads[3].join();
+        System.out.println((double) System.currentTimeMillis() - executionTime);
+
+        createArray(new double[1_000_000]);
     }
 
-    public synchronized static void createMultiThreadArray(double[] arr) throws InterruptedException {
-        long executionTime = System.currentTimeMillis();
-
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 250_000; i++) {
-                arr[i] = i + 1;
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            for (int i = 250_000; i < 500_000; i++) {
-                arr[i] = i + 1;
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
-
-        Thread thread3 = new Thread(() -> {
-            for (int i = 500_000; i < 750_000; i++) {
-                arr[i] = i + 1;
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
-
-        Thread thread4 = new Thread(() -> {
-            for (int i = 750_000; i < 1_000_000; i++) {
-                arr[i] = i + 1;
-                arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread1.join();
-        thread2.join();
-        thread3.join();
-        thread4.join();
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println((double) System.currentTimeMillis() - executionTime);
+    public static void fillDoubleArray(double[] result, int start, int end) {
+        for (int i = start; i < end; i++) {
+            result[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
     }
 
     public static void createArray(double[] arr) {
         long executionTime = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
             arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
         }
-        System.out.println(Arrays.toString(arr));
         System.out.println((double) System.currentTimeMillis() - executionTime);
-    }
-
-    @Override
-    public void run() {
     }
 }
